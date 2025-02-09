31c428a0a6d1389cda280e192c7164861cd9469d
pinyirao <prao2@rrc.ca>
2025-02-09 17:27:57 -0600

    feat: finish test cases for joi validation for branch management endpoints(put, post)

--- test/branchRoutes.test.ts
+++ test/branchRoutes.test.ts
@@ -41,6 +41,35 @@ describe("Branch Routes", () => {
       expect(response.body.data).toHaveProperty("address");
       expect(response.body.data.id).toBeGreaterThan(0);
     });
+    it("should receive an error when name length is less than 3", async () => {
+      const response: Response = await request(app)
+        .post("/api/v1/branches")
+        .send({
+          name: "Re",
+          address: "St. James 1111",
+          phone: "123-456-7890",
+        });
+
+      // check the status
+      expect(response.status).toBe(400);
+      // check han return an object containing message
+      expect(response.body).toHaveProperty("error");
+      expect(response.body.error).toMatch("should greater than 3");
+    });
+    it("should receive an error when address field is missing", async () => {
+      const response: Response = await request(app)
+        .post("/api/v1/branches")
+        .send({
+          name: "Red River Branch",
+          phone: "123-456-7890",
+        });
+
+      // check the status
+      expect(response.status).toBe(400);
+      // check han return an object containing message
+      expect(response.body).toHaveProperty("error");
+      expect(response.body.error).toMatch(/address is required/i);
+    });
   });
   describe("PUT /api/v1/branches/:id", () => {
     it("should update a exsiting branch object", async () => {
@@ -58,6 +87,35 @@ describe("Branch Routes", () => {
       expect(response.body.data).toHaveProperty("phone");
       // check the updated address value
       expect(response.body.data.address).toBe("St. James 1111");
+    });
+    it("should receive an error if has the name field", async () => {
+      const response: Response = await request(app)
+        .put("/api/v1/branches/2")
+        .send({
+          name: "Red River Branch",
+          address: "St. James 1111",
+          phone: "123-456-7890",
+        });
+
+      // check the status
+      expect(response.status).toBe(400);
+      // check han return an object containing message
+      expect(response.body).toHaveProperty("error");
+      expect(response.body.error).toMatch(/is not allowed to be updated/i);
+    });
+    it("should receive an error if phone is an empty string", async () => {
+      const response: Response = await request(app)
+        .put("/api/v1/branches/2")
+        .send({
+          address: "St. James 1111",
+          phone: "",
+        });
+
+      // check the status
+      expect(response.status).toBe(400);
+      // check han return an object containing message
+      expect(response.body).toHaveProperty("error");
+      expect(response.body.error).toMatch(/cannot be empty/i);
     });
   });
   describe("DELETE /api/v1/branches/:id", () => {
