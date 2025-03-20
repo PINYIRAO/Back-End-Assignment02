004cb388220a6e8b63dd90a9ab1619c588f1ce8a
pinyirao <prao2@rrc.ca>
2025-03-20 13:31:07 -0500

    openapi: add api documentation for models

--- src/api/v1/models/employeeModel.ts
+++ src/api/v1/models/employeeModel.ts
@@ -1,3 +1,32 @@
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Employee:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description:  the employee id
+ *         name:
+ *           type: string
+ *           description:  the employee name
+ *         position:
+ *           type: string
+ *           description:  the employee position
+ *         department:
+ *           type: string
+ *           description:  the employee department
+ *         email:
+ *           type: string
+ *           description:  the employee email
+ *         phone:
+ *           type: string
+ *           description:  the employee phone
+ *         branchId:
+ *           type: string
+ *           description:  the employee's branchId
+ */
 export interface Employee {
   id: string;
   name: string;
